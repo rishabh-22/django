a61c2459cee353c3f6997a76dbcd7a1f8081e94f
rishabh-22 <rishabh.bh22@gmail.com>
2019-03-25 13:42:56 +0530

    changes

--- ProjectFile/myproject/boards/views.py
+++ ProjectFile/myproject/boards/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.http import HttpResponse
+from django.views import View
 import datetime
 
-# Create your views here.
+#
+ Create your views here.
 
 def home(request):
-	# return HttpResponse('Hello World!')
 	now = datetime.datetime.now()
 	hr = now.hour
-	print (hr)
-	# return HttpResponse(hr)
+	
 	if(hr>=5 and hr<12):
-		return HttpResponse("Good Morning")
-	if (hr>=12 and hr<17):
-		return HttpResponse("Good Afternoon")
-	if (hr>=17 and hr<21):
-		return HttpResponse("Good Evening")
-	if (hr >=21 and hr<=1):
-		return HttpResponse("Good Night")
+		message = "Good Morning"
+	elif (hr>=12 and hr<17):
+		message = "Good Afternoon"
+	elif (hr>=17 and hr<21):
+		message = "Good Evening"
+	elif (hr >=21 and hr<24):
+		message = "Good Night"
 	else:
-		return HttpResponse("IDK what to say! go to bed.")
+		message = "IDK what to say! go to bed."
+		
+	return message

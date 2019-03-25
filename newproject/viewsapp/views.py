b5be4084c86b1f0ca14a7351e080eb6f7e2da0f6
rishabh-22 <rishabh.bh22@gmail.com>
2019-03-25 13:37:15 +0530

    changes

--- newproject/viewsapp/views.py
+++ newproject/viewsapp/views.py
@@ -7,12 +7,14 @@ def index(request):
 	# print(request)
 	return render(request, 'index.html')
 def second(request, ids):
-	# return HttpResponse(ids)
-	if (ids > 6 and ids < 12):
-		return HttpResponse("Good morning")
-	if (ids > 12 and ids < 18):
-		return HttpResponse("Good afternoon")
-	if (ids > 18 and ids < 24):
-		return HttpResponse("Good night")
+	
+	if (ids >= 6 and ids < 12):
+		message = "Good morning!"
+	elif (ids >= 12 and ids < 18):
+		message = "Good afternoon"
+	elif (ids >= 18 and ids < 24):
+		message ="Good night"
 	else:
-		return HttpResponse("Go to bed")
+		# return HttpResponse("Go to bed")
+		message = "Go to bed"
+	return HttpResponse(message)

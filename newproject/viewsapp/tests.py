a2f487c352b795c631a9145179df9c862eee2b95
rishabh-22 <rishabh.bh22@gmail.com>
2019-04-14 12:18:16 +0530

    exercise for unit testing

--- newproject/viewsapp/tests.py
+++ newproject/viewsapp/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.http import HttpResponse
+from .views import second
+from  django.test.client import RequestFactory
+import unittest
 
 # Create your tests here.
+
+class TestGreeting(unittest.TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_one(self):
+        """pass the test"""
+        request = self.factory.get('')
+        response = second(request, 11)
+        self.assertEqual(response, 'Good morning!') 
+
+    def test_two(self):
+        """pass the test"""
+        request = self.factory.get('')
+        response = second(request, 17)
+        self.assertEqual(response, 'Good afternoon')
+
+    def test_three(self):
+        """pass the test"""
+        request = self.factory.get('')
+        response = second(request, 20)
+        self.assertNotEqual(response, "Good night")
+
+    def test_four(self):
+        """Supposed to fail"""
+        request = self.factory.get('')
+        response = second(request, 5)
+        self.assertEqual(response, "Good night")
+
+
